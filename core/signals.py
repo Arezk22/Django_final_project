dd03d683c458fdddcb48dc9e46db69b2ac624292
Arezk22 <ahmed60401201818966@gmail.com>
2026-05-30 18:50:57 +0300

    core/signals.py && core/Forms.py : update code to save user information if register from APIs or UI form in template(form.html) mvt

--- core/signals.py
+++ core/signals.py
@@ -11,5 +11,5 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-
-    instance.profile.save()
+    if not kwargs.get('created') and hasattr(instance, 'profile'):
+        instance.profile.save()
